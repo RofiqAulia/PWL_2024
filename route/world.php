570aae01ded51e2af240c1ae6af49b721d651166
= <=>
2024-02-21 13:04:54 +0700

    first commit

---
+++ route/world.php
@@ -0,0 +1,4 @@
+use Illuminate\Support\Facades\Route;
+Route::get('/world', function () {
+return 'World';
+});
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+use Illuminate\Support\Facades\Route;
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
