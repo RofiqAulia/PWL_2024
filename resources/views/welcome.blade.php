0eab2d7cbb84a8d8b5e1a8a039caac0b40078457
= <=>
2024-02-22 10:17:24 +0700

    hjhdbhj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,5 +129,19 @@
                 </div>
             </div>
         </div>
+    </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Perograman WEb Lanjut 2024</h2>
+        <h3>Semangat Belajar Laravel 10</h3>
+
     </body>
 </html>
