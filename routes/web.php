a7acbbb993031b1f1a471786ca765e064db61d71
= <=>
2024-02-27 17:41:34 +0700

    commit rofiq

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,8 @@ Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID'.$id;
 });
 
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
     
