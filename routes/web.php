de5e1656ec4784e73451fa7319b13c23fe1d3a5b
= <=>
2024-02-27 23:37:20 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,8 @@ Route::get('/articles/{id}',[PageController::class,'articles']);
 Route::get('/',[HomeController::class,'index']);
 Route::get('/about',[AboutController::class,'about']);
 Route::get('/articles/{id}',[ArticleController::class,'articles']);
+
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
