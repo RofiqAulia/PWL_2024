eb4bbf38b3806258abbbe60e0f2215ebffdc2eec
= <=>
2024-02-27 23:38:09 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,8 @@ Route::get('/articles/{id}',[ArticleController::class,'articles']);
 use App\Http\Controllers\PhotoController;
 
 Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+]);
