b51f5720ce290787383f9df7f53a89bb7512ac54
= <=>
2024-02-27 17:47:54 +0700

    ubah ke 9

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::get('/articles/{id}', function ($id) {
     return 'Halaman Artikel dengan ID'.$id;
 });
 
-Route::get('/user/{name?}', function ($name='john') {
+Route::get('/user/{name?}', function ($name=null) {
     return 'Nama saya '.$name;
 });
 
